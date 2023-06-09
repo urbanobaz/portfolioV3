5df9fe711c7b15006996d9bad9e1767283d818b2
Urbano <urbanobaz@yahoo.com>
2023-06-09 09:53:41 -0400

    Progress on navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import PalmTree from '/public/favicon.ico';
 import { cn } from '@/app/helpers/utils';
 import styles from './Navbar.module.css';
@@ -10,40 +11,62 @@ import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
   return (
-    <nav className="mx-10 h-14 left-0 flex justify-between lg:mx-1/8">
-      <Link
-        className="flex justify-center flex-col text-2xl cursor-pointer"
-        href={'/'}
-      >
-        <Image height={30} src={PalmTree} alt={'Logo'} />
-      </Link>
+    <>
+      <nav className="mx-10 h-14 left-0 flex justify-between lg:mx-1/8">
+        <Link
+          className="flex justify-center flex-col text-2xl cursor-pointer"
+          href={'/'}
+        >
+          <Image height={30} src={PalmTree} alt={'Logo'} />
+        </Link>
 
-      <div
-        className={cn('flex justify-center flex-col', styles.linksContainer)}
-      >
-        <ul>
-          <Link className="mr-4 cursor-pointer" href={'/projects/1'}>
-            Project 1
-          </Link>
+        <div
+          className={cn('flex justify-center flex-col', styles.linksContainer)}
+        >
+          <ul>
+            <Link className="mr-4 cursor-pointer" href={'/projects/1'}>
+              Project 1
+            </Link>
 
-          <Link className="mr-4 cursor-pointer" href={'/projects/2'}>
-            Project 2
-          </Link>
-          <Link className="cursor-pointer" href={'/projects/3'}>
-            Project 3
-          </Link>
-        </ul>
-      </div>
+            <Link className="mr-4 cursor-pointer" href={'/projects/2'}>
+              Project 2
+            </Link>
+            <Link className="cursor-pointer" href={'/projects/3'}>
+              Project 3
+            </Link>
+          </ul>
+        </div>
+        <button
+          className={cn(
+            'flex justify-center flex-col',
+            styles.linksIconContainer
+          )}
+          onClick={() => setDropdownVisible(!dropdownVisible)}
+        >
+          <FontAwesomeIcon icon={faBarsStaggered}></FontAwesomeIcon>
+        </button>
+      </nav>
       <div
         className={cn(
-          'flex justify-center flex-col',
-          styles.linksIconContainer
+          'flex flex-col px-10 w-full',
+          styles.dropdownContainer,
+          !dropdownVisible && styles.disabled
         )}
       >
-        <FontAwesomeIcon icon={faBarsStaggered}></FontAwesomeIcon>
+        <Link className="mr-4 cursor-pointer" href={'/projects/1'}>
+          Project 1
+        </Link>
+
+        <Link className="mr-4 cursor-pointer" href={'/projects/2'}>
+          Project 2
+        </Link>
+        <Link className="cursor-pointer" href={'/projects/3'}>
+          Project 3
+        </Link>
       </div>
-    </nav>
+    </>
   );
 };
 
