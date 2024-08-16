b29d183f8fb01e9a593571b24920c7aa3ee53b9e
Urbano <urbano@Urbanos-MBP.attlocal.net>
2024-08-16 17:59:14 -0400

    Adding navbar fix for projects page bug

--- app/projects/2/page.tsx
+++ app/projects/2/page.tsx
@@ -1,59 +1,62 @@
-import Container from '@/components/container/Container';
+import Navbar from '@/components/navbar/Navbar';
 import Image from 'next/image';
 
 export default function BooksPage() {
   return (
-    <Container>
-      <h2 className="text-2xl font-semibold mb-10">Books & More</h2>
-      <p className="mb-10">
-        NextJS project that lets you keep track of the books in your collection.
-        Uses the Google Books API to bring in book information when you add a
-        book. Clerk for authentication, Vercel for hosting. Check out the{' '}
-        <a
-          href="https://futureinblockchain.com"
-          className="text-blue-600 hover:font-medium"
-        >
-          live site
-        </a>{' '}
-        or the Github{' '}
-        <a
-          href="https://github.com/urbanobaz/next13-mongo"
-          className="text-blue-600 hover:font-medium"
-        >
-          repository
-        </a>
-        .
-      </p>
+    <div className='flex flex-col'>
+      <Navbar />
+      <div className='px-[34px] mt-[100px] max-w-[980px] w-full mx-auto lg:px-0 lg:mt-40'>
+        <h2 className="text-2xl font-semibold mb-10">Books & More</h2>
+        <p className="mb-10">
+          NextJS project that lets you keep track of the books in your collection.
+          Uses the Google Books API to bring in book information when you add a
+          book. Clerk for authentication, Vercel for hosting. Check out the{' '}
+          <a
+            href="https://futureinblockchain.com"
+            className="text-blue-600 hover:font-medium"
+          >
+            live site
+          </a>{' '}
+          or the Github{' '}
+          <a
+            href="https://github.com/urbanobaz/next13-mongo"
+            className="text-blue-600 hover:font-medium"
+          >
+            repository
+          </a>
+          .
+        </p>
 
-      <h2 className="text-md font-semibold">Still to do:</h2>
-      <ul className="flex flex-col list-disc ml-6">
-        <li className="line-through">Add authentication</li>
-        <li className="line-through">
-          Set up database to store books for each user account
-        </li>
-        <li>Improve design</li>
-      </ul>
+        <h2 className="text-md font-semibold">Still to do:</h2>
+        <ul className="flex flex-col list-disc ml-6">
+          <li className="line-through">Add authentication</li>
+          <li className="line-through">
+            Set up database to store books for each user account
+          </li>
+          <li>Improve design</li>
+        </ul>
 
-      <p className="mt-10">
-        <strong className="font-semibold">Technologies Used:</strong> React,
-        TypeScript, NextJS, MongoDB, Clerk, Vercel, TailwindCSS, Google Books
-        API
-      </p>
+        <p className="mt-10">
+          <strong className="font-semibold">Technologies Used:</strong> React,
+          TypeScript, NextJS, MongoDB, Clerk, Vercel, TailwindCSS, Google Books
+          API
+        </p>
 
-      <Image
-        src="/shots-books.png"
-        alt={'Project image hightlight'}
-        width={980}
-        height={750}
-        className="mt-5 hidden rounded-xl sm:block"
-      />
-      <Image
-        src="/books-small.png"
-        alt={'Project image hightlight'}
-        width={980}
-        height={750}
-        className="mt-5 block rounded-xl sm:hidden"
-      />
-    </Container>
+        <Image
+          src="/shots-books.png"
+          alt={'Project image hightlight'}
+          width={980}
+          height={750}
+          className="mt-5 hidden rounded-xl sm:block"
+        />
+        <Image
+          src="/books-small.png"
+          alt={'Project image hightlight'}
+          width={980}
+          height={750}
+          className="mt-5 block rounded-xl sm:hidden"
+        />
+      </div>
+    </div>
   );
 }
--- app/projects/3/page.tsx
+++ app/projects/3/page.tsx
@@ -1,47 +1,50 @@
-import Container from '@/components/container/Container';
+import Navbar from '@/components/navbar/Navbar';
 import Image from 'next/image';
 
 export default function PortfolioPage() {
   return (
-    <Container>
-      <h2 className="text-2xl font-semibold mb-10">Portfolio</h2>
-      <p className="mb-5">
-        NextJS project highlighting a little bit more information about myself.
-        Uses the new Next 13 app directory. Check out the{' '}
-        <a
-          href="https://www.urbanobaz.com/"
-          className="text-blue-600 hover:font-medium"
-        >
-          live site
-        </a>{' '}
-        or the Github{' '}
-        <a
-          href="https://github.com/urbanobaz/portfolioV3"
-          className="text-blue-600 hover:font-medium"
-        >
-          repository
-        </a>
-        .
-      </p>
+    <div className='flex flex-col'>
+      <Navbar />
+      <div className='px-[34px] mt-[100px] max-w-[980px] w-full mx-auto lg:px-0 lg:mt-40'>
+        <h2 className="text-2xl font-semibold mb-10">Portfolio</h2>
+        <p className="mb-5">
+          NextJS project highlighting a little bit more information about myself.
+          Uses the new Next 13 app directory. Check out the{' '}
+          <a
+            href="https://www.urbanobaz.com/"
+            className="text-blue-600 hover:font-medium"
+          >
+            live site
+          </a>{' '}
+          or the Github{' '}
+          <a
+            href="https://github.com/urbanobaz/portfolioV3"
+            className="text-blue-600 hover:font-medium"
+          >
+            repository
+          </a>
+          .
+        </p>
 
-      <p>
-        <strong className="font-semibold">Technologies Used:</strong> React,
-        TypeScript, TailwindCSS, NextJS
-      </p>
-      <Image
-        src="/portfolio.png"
-        alt={'Project image hightlight'}
-        width={980}
-        height={750}
-        className="mt-5 hidden rounded-xl sm:block"
-      />
-      <Image
-        src="/portfolio-small.png"
-        alt={'Project image hightlight'}
-        width={980}
-        height={750}
-        className="mt-5 block rounded-xl sm:hidden"
-      />
-    </Container>
+        <p>
+          <strong className="font-semibold">Technologies Used:</strong> React,
+          TypeScript, TailwindCSS, NextJS
+        </p>
+        <Image
+          src="/portfolio.png"
+          alt={'Project image hightlight'}
+          width={980}
+          height={750}
+          className="mt-5 hidden rounded-xl sm:block"
+        />
+        <Image
+          src="/portfolio-small.png"
+          alt={'Project image hightlight'}
+          width={980}
+          height={750}
+          className="mt-5 block rounded-xl sm:hidden"
+        />
+      </div>
+    </div>
   );
 }
